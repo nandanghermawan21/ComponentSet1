c1cedd046129dc0be709178f612d1d48dd3b81de
Renoldi Boz <renoldiboz@Renoldis-MacBook-Pro.local>
2019-04-18 17:23:06 +0700

    change menu

--- example/lib/MenuButton.dart
+++ example/lib/MenuButton.dart
@@ -17,14 +17,14 @@ class MenuButtonState extends State<MenuButton> {
     Color colorIcon = const Color(0xff5555BF),
     double sizeIcon = 27.2,
     Color colorBedge = const Color(0xffFF9500),
-    String textBedge = "1",
+    int textBedge = 0,
     Color colorTextBedge = Colors.white,
-    String textMenu = 'Notification',
+    String textMenu = '',
     Color colorRxtMenu = const Color(0xFF707070),
     double sizeTextMenu = 11,
   }) {
     return Container(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
       width: sizeMenuButton,
       height: sizeMenuButton,
       decoration: BoxDecoration(
@@ -35,18 +35,20 @@ class MenuButtonState extends State<MenuButton> {
         children: <Widget>[
           Align(
               alignment: Alignment.topRight,
-              child: Container(
-                margin: EdgeInsets.all(10),
-                width: sizeMenuButton / 4,
-                height: sizeMenuButton / 4,
-                decoration: BoxDecoration(
-                  color: colorBedge,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                    child: Text(textBedge,
-                        style: TextStyle(color: colorTextBedge))),
-              )),
+              child: textBedge == 0
+                  ? Container()
+                  : Container(
+                      margin: EdgeInsets.all(10),
+                      width: sizeMenuButton / 4,
+                      height: sizeMenuButton / 4,
+                      decoration: BoxDecoration(
+                        color: colorBedge,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                          child: Text("$textBedge",
+                              style: TextStyle(color: colorTextBedge))),
+                    )),
           Align(
               alignment: Alignment.center,
               child: Icon(
@@ -76,31 +78,59 @@ class MenuButtonState extends State<MenuButton> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            // color: Colors.greenAccent,
-            margin: EdgeInsets.only(top: 10, bottom: 10),
-            child: Wrap(
-              alignment: WrapAlignment.start,
-              spacing: 4,
-              children: <Widget>[
-                menu(iconMenu: IconDataRegular(FontAwesomeId.fa_acorn)),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-                menu(),
-              ],
+          Center(
+            child: Container(
+              // color: Colors.grey,
+              padding: EdgeInsets.all(20),
+              alignment:
+                  MediaQuery.of(context).orientation == Orientation.portrait
+                      ? Alignment.center
+                      : Alignment.topLeft,
+              margin: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? EdgeInsets.all(10)
+                  : EdgeInsets.fromLTRB(35, 10, 35, 10),
+              child: Wrap(
+                spacing: 10,
+                alignment: WrapAlignment.start,
+                children: <Widget>[
+                  menu(
+                      textMenu: 'Notifikasi',
+                      iconMenu: IconDataLight(FontAwesomeId.fa_acorn),
+                      textBedge: 1),
+                  menu(
+                      textMenu: 'Job',
+                      iconMenu: IconDataLight(FontAwesomeId.fa_tasks)),
+                  menu(
+                      textMenu: 'Route',
+                      iconMenu: IconDataLight(FontAwesomeId.fa_map_marked)),
+                  menu(
+                      textMenu: 'Job',
+                      iconMenu: IconDataLight(FontAwesomeId.fa_file_alt)),
+                  menu(
+                      textMenu: 'Setting',
+                      iconMenu: IconDataLight(FontAwesomeId.fa_cog)),
+                ],
+              ),
             ),
           ),
-          Text("dagdgsgsdg")
+          Text("dagdgsgsdg"),
+          // Container(
+          //   height: 300,
+          //   width: 300,
+          //   decoration: BoxDecoration(
+          //     gradient: LinearGradient(
+          //       begin: Alignment.center,
+          //       end: Alignment(
+          //           1, 0.1), // 10% of the width, so there are ten blinds.
+          //       colors: [
+          //         const Color(0xFF665EFF),
+          //         const Color(0xFFFF4F9A)
+          //       ], // whitish to gray
+          //       tileMode:
+          //           TileMode.clamp, // repeats the gradient over the canvas
+          //     ),
+          //   ),
+          // )
         ],
       ),
     );
