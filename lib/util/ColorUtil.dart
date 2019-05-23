4ee6d4263bef5491dffe530e040707aaf287e09b
Renoldi Boz <renoldiboz@Renoldis-MacBook-Pro.local>
2019-05-23 09:10:50 +0700

    add color util

---
+++ lib/util/ColorUtil.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class HolcimBetonColor {
+  Color colorGreen = Color(0xFF3CD58E);
+  Color borderInputGray = Color(0xFFCDCDD1);
+  Color mainColor = Color(0xFFA8C700);
+  Color yellowColor = Color(0xFFFFB426);
+  Color greenColor = Color(0xFF3CD58E);
+  Color redColor = Color(0xFFFF5966);
+  Color scafoldColor = Color(0xFF565657);
+  Color inputTextBackground = Color(0xFFF7F7F7);
+  Color yellow = Color(0xFFFFC61C);
+  Color green = Color(0xFFA8C700);
+}
