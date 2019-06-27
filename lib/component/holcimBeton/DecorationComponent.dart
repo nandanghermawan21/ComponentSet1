a4db7d9c43dd0db1973fb6c512f4c8a347e40163
Renoldi Boz <renoldiboz@Renoldis-MacBook-Pro.local>
2019-06-27 11:42:53 +0700

    add decoration holcim

---
+++ lib/component/holcimBeton/DecorationComponent.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:component_set1/util/ColorUtil.dart';
+
+class DecorationComponent {
+  static BoxDecoration boxDecoration({bool dark = true}) {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [0.2, 8],
+        colors: [
+          dark == true ? HolcimBetonColor().greyColor : Colors.white,
+          HolcimBetonColor().blackColor,
+        ],
+      ),
+      image: DecorationImage(
+        image: AssetImage(
+          dark == true
+              ? "assets/backgorundDark.png"
+              : "assets/backgorundLight.png",
+        ),
+        fit: BoxFit.fitWidth,
+        alignment: Alignment.topCenter,
+      ),
+    );
+  }
+}
