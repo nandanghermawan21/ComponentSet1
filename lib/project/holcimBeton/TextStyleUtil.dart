37afd1b23a6ec878b5cc82839ba9dcbd12687863
nandang <nandanghermawan21@gmail.com>
2019-07-02 13:53:51 +0700

    add style label text

--- lib/project/holcimBeton/TextStyleUtil.dart
+++ lib/project/holcimBeton/TextStyleUtil.dart
@@ -1,4 +1,5 @@
 import 'package:component_set1/custom/material_custom.dart';
+import 'package:component_set1/project/holcimBeton/ColorUtil.dart';
 import 'FontUtil.dart';
 
 class TextStyleUtil {
@@ -6,7 +7,7 @@ class TextStyleUtil {
     return TextStyle(
         color: Colors.white,
         fontFamily: FontUtil().primary,
-        fontSize: 28,
+        fontSize: FontUtil().xl,
         fontWeight: FontWeight.bold);
   }
 
@@ -17,4 +18,13 @@ class TextStyleUtil {
         fontSize: 16,
         fontWeight: FontWeight.normal);
   }
+
+  static TextStyle mainLabel() {
+    return TextStyle(color: Colors.white, fontSize: FontUtil().l);
+  }
+
+  static TextStyle linkLabel() {
+    return TextStyle(color: ColorUtil().mainColor, fontSize: FontUtil().l);
+  }
+
 }
