b728faee0dda13f70ce5bd31549c4811b100cede
nandang <nandanghermawan21@gmail.com>
2019-07-30 22:04:29 +0700

    commit

--- example/lib/module/holcimBeton/view.dart
+++ example/lib/module/holcimBeton/view.dart
@@ -3,7 +3,10 @@ import 'package:example/component/containerComponent.dart';
 import 'package:flutter/material.dart';
 import 'presenter.dart';
 import 'package:component_set1/component/InputComponent.dart';
+import 'package:component_icons/font_awesome.dart';
 import 'package:component_set1/project/holcimBeton/BottonComponent.dart';
+import 'package:component_set1/project/holcimBeton/module/menu/main.dart'
+    as menu;
 
 class View extends PresenterState {
   @override
@@ -20,7 +23,7 @@ class View extends PresenterState {
                     print("tab the main button");
                   })),
           ContainerComponent.componentBox(
-              title: "MainButton",
+              title: "BoxDecoration",
               height: 700,
               background: Colors.green,
               component: Container(
@@ -28,6 +31,42 @@ class View extends PresenterState {
                 height: 700 - 50.0,
                 // color: Colors.red,
                 decoration: DecorationComponent.boxDecoration(),
+              )),
+          ContainerComponent.componentBox(
+              title: "Menu",
+              height: 400,
+              background: Colors.green,
+              component: Container(
+                padding: EdgeInsets.only(top: 20),
+                width: double.infinity,
+                height: 400 - 50.0,
+                // color: Colors.red,
+                decoration: DecorationComponent.boxDecoration(),
+                child: menu.Presenter(
+                  menus: <menu.HolcimBetonMenuModel>[
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "Notification",
+                        icon: FontAwesomeRegular(FontAwesomeId.fa_bell)),
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "Job Order",
+                        icon:
+                            FontAwesomeRegular(FontAwesomeId.fa_file_invoice)),
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "Shipment",
+                        icon:
+                            FontAwesomeRegular(FontAwesomeId.fa_shipping_fast)),
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "live Maps",
+                        icon: FontAwesomeRegular(FontAwesomeId.fa_map_marked)),
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "Pump",
+                        icon: FontAwesomeRegular(FontAwesomeId.fa_gas_pump)),
+                    menu.HolcimBetonMenuModel(
+                        textMenu: "Emergency",
+                        icon:
+                            FontAwesomeRegular(FontAwesomeId.fa_lightbulb_on)),
+                  ],
+                ),
               )),
         ],
       ),
